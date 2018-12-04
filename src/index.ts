049a660eab79f398632d4dd387ef9135b7ab46d4
TomShacham <shachamtom@gmail.com>
2018-12-04 08:40:14 +0000

    take input

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,18 @@ import { Cell, Snake } from "./Snake";
 (async function main() {
   const [width, height] = process.argv.splice(2);
 
-  const game = new SnakeGame(Number(width), Number(height), new Snake([new Cell(0, 2), new Cell(0, 1)]));
+  const game = new SnakeGame(
+    Number(width),
+    Number(height),
+    new Snake([new Cell(0, 2), new Cell(0, 1)]));
+
+  process.stdin.resume();
+  process.stdin.setRawMode!(true);
+  process.stdin.on('data', function (chunk) {
+    console.log('received: ', chunk.toString());
+    if (chunk.toString() === 'q') process.exit();
+  });
 
   const refreshRate = 1000;
-
   setInterval(() => console.log(game.print()), refreshRate);
 })();
